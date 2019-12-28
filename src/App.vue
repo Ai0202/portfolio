5b4d58b13e59a7d80d907657dd54bde068bba3ed
Atsushi <atsushi88885@gmail.com>
2019-12-28 18:04:40 +0900

    create footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <v-header />
+    <v-footer />
   </v-app>
 </template>
 
 <script>
 import VHeader from "@/components/Header";
+import VFooter from "@/components/Footer";
 
 export default {
   name: "App",
   components: {
-    VHeader
+    VHeader,
+    VFooter
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-footer absolute>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} —
+      <strong>Daaike</strong>
+    </v-col>
+  </v-footer>
+</template>
